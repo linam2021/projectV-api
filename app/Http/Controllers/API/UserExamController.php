59406cec81282a6d1702f1b1953d6c33e1f32f45
unknown <lina.m.2021.m@gmail.com>
2021-09-09 16:07:07 +0300

    change ExamController name to UserExamController

--- app/Http/Controllers/API/UserExamController.php
+++ app/Http/Controllers/API/UserExamController.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
-class ExamController extends BaseController
+class UserExamController extends BaseController
 {
     public function examInfo(){
         try {
