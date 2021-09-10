3e3d4941202d3cba72c8f2df980ca36112f90341
unknown <lina.m.2021.m@gmail.com>
2021-09-10 07:45:30 +0300

    resolve PostgreSQL bug in userPath model

--- app/Models/UserPath.php
+++ app/Models/UserPath.php
@@ -12,7 +12,7 @@ class UserPath extends Model
 
     // Define Table User_Path
     protected $table = 'user_path';
-    //protected $primaryKey = ['user_id', 'path_id', 'path_start_date'];
+    protected $primaryKey = ['user_id', 'path_id', 'path_start_date'];
 
     protected $fillable = [
         'user_id', 'path_id', 'path_start_date', 'user_status', 'repeat_chance_no', 'score', 'answer_join', 'answer_accept_order',
