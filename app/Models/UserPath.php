6b40c5a6b72bd48e3e74162c42677d9b934f9f2a
unknown <lina.m.2021.m@gmail.com>
2021-09-10 07:47:17 +0300

    resolve PostgreSQL bug in userPath model

--- app/Models/UserPath.php
+++ app/Models/UserPath.php
@@ -10,6 +10,8 @@ class UserPath extends Model
 {
     use HasFactory;
 
+    public $incrementing = false;
+    public $keyType = 'string';
     // Define Table User_Path
     protected $table = 'user_path';
     protected $primaryKey = ['user_id', 'path_id', 'path_start_date'];
