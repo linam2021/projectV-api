d96c3b49019ef13b98666c1f1e55072d2cfa37d3
unknown <lina.m.2021.m@gmail.com>
2021-09-10 07:43:05 +0300

    resolve PostgreSQL bug in userPath model

--- app/Models/UserPath.php
+++ app/Models/UserPath.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class UserPath extends Model
 {
@@ -11,9 +12,16 @@ class UserPath extends Model
 
     // Define Table User_Path
     protected $table = 'user_path';
-    protected $primaryKey = ['user_id', 'path_id', 'path_start_date'];
+    //protected $primaryKey = ['user_id', 'path_id', 'path_start_date'];
 
     protected $fillable = [
         'user_id', 'path_id', 'path_start_date', 'user_status', 'repeat_chance_no', 'score', 'answer_join', 'answer_accept_order',
     ];
+
+    protected function setKeysForSaveQuery($query)
+    {
+        return $query->where('user_id', $this->getAttribute('user_id'))
+                     ->where('path_id', $this->getAttribute('path_id'))
+                     ->where('path_start_date', $this->getAttribute('path_start_date'));
+    }
 }
