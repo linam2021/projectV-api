a86304df30aee71e9cc6f439925ed01bf94b844c
unknown <lina.m.2021.m@gmail.com>
2021-09-13 22:05:05 +0300

    resolve PostgreSQL bug in exam_user model

--- app/Models/UserExam.php
+++ app/Models/UserExam.php
@@ -8,10 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class UserExam extends Model
 {
     use HasFactory;
+
+    public $incrementing = false;
+    public $keyType = 'string';
     // Define Table User_Path
     protected $table = 'user_exam';
+    protected $primaryKey = ['user_id', 'path_id', 'path_start_date','exam_id'];
+
     protected $fillable = ['user_id','path_id','path_start_date','exam_id','user_exam_date','exam_result','is_well_prepared','is_easy_exam'];
 
+    protected function setKeysForSaveQuery($query)
+    {
+        return $query->where('user_id', $this->getAttribute('user_id'))
+                     ->where('path_id', $this->getAttribute('path_id'))
+                     ->where('path_start_date', $this->getAttribute('path_start_date'))
+                     ->where('exam_id', $this->getAttribute('exam_id'));
+    }
+    
     //define relqtionship between user_exam and exams tables
 
     public function exam()
