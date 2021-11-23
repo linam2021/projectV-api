e700082278ee3552e702d56bf4b312c5dd5143e0
unknown <lina.m.2021.m@gmail.com>
2021-11-23 23:28:10 +0200

    modify path and exam controllers and views

--- database/seeders/PathTableSeeder.php
+++ database/seeders/PathTableSeeder.php
@@ -18,25 +18,26 @@ class PathTableSeeder extends Seeder
         $now = Carbon::now()->toDateTimeString();
         DB::table('paths')->insert([
             'path_name' => 'Database',
-            'current_stage' => 2,
+            'current_stage' => 0,
             'questionbank_path_id'=>100,
             'path_image_name'=>'database&api.jpg',
+            'path_start_date'=>Carbon::parse(Carbon::now()->addDays(7))->format('Y-m-d'),
             'created_at' => $now,
             'updated_at' => $now
         ]);
         DB::table('paths')->insert([
             'path_name' => 'Flutter',
-            'current_stage' => 1,
+            'current_stage' => 0,
             'questionbank_path_id'=>200,
-            'created_at' => $now,
+            'path_start_date'=>Carbon::parse(Carbon::now()->addDays(3))->format('Y-m-d'),
             'path_image_name'=>'flutter.jpg',
+            'created_at' => $now,
             'updated_at' => $now
         ]);
         DB::table('paths')->insert([
             'path_name' => 'design',
             'current_stage' => 0,
             'questionbank_path_id'=>300,
-            'path_start_date'=>Carbon::parse(Carbon::now()->addDays(10))->format('Y-m-d'),
             'path_image_name'=>'Design.jpg',
             'created_at' => $now,
             'updated_at' => $now
@@ -45,7 +46,6 @@ class PathTableSeeder extends Seeder
             'path_name' => 'Web',
             'current_stage' => 0,
             'questionbank_path_id'=>400,
-            'path_start_date'=>Carbon::parse(Carbon::now()->addDays(5))->format('Y-m-d'),
             'path_image_name'=>'web.jpg',
             'created_at' => $now,
             'updated_at' => $now
